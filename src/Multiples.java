85d37f62337152026f2286053a3886fbd63d9184
Your Name <shiqi.zuo@mail.utoronto.ca>
2023-09-13 02:07:45 -0400

    refactored to extract helper method

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Multiples {
-}
+public class Multiples {
+        public static void main (String[]args){
+        int m = 0;
+        for (int i = 1; i < 1000; i++) {
+
+            // Find out which numbers divide i.
+            boolean divisibleBy3 = i % 3 == 0;
+            boolean divisibleBy5 = i % 5 == 0;
+
+            // Print our appropriate result.
+            if (divisibleBy3 || divisibleBy5) {
+
+                m += 1;
+
+            } else {
+
+                m += 0;
+
+            }
+        }
+        System.out.println(m);
+    }
+    }
--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,23 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main (String[]args){
+        int m = 100;
+        int i = 0;
+        while (m != 0) {
+
+            // Find out which numbers divide i.
+            boolean divisibleBy2 = m % 2 == 0;
+
+            // Print our appropriate result.
+            if (divisibleBy2) {
+
+                m = m / 2;
+
+            } else {
+                m = 0;
+                i += 1;
+            }
+            i += 1;
+        }
+        System.out.println(i);
+    }
 }
